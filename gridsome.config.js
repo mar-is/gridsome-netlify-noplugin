116d5e3d307fb357bdbfda58edee6da3c292303e
Marasi <marasi.mbp@gmail.com>
2020-04-03 12:10:23 +0700

    Init repository

---
+++ gridsome.config.js
@@ -0,0 +1,49 @@
+// This is where project configuration and plugin options are located.
+// Learn more: https://gridsome.org/docs/config
+
+// Changes here require a server restart.
+// To restart press CTRL + C in terminal and run `gridsome develop`
+
+module.exports = {
+	siteName: "Gridsome",
+	transformers: {
+		remark: {
+			externalLinksTarget: "_blank",
+			externalLinksRel: ["nofollow", "noopener", "noreferrer"],
+			anchorClassName: "icon icon-link",
+			plugins: [
+				// ...global plugins
+			]
+		}
+	},
+
+	plugins: [
+		{
+			use: "@gridsome/source-filesystem",
+			options: {
+				path: "contents/authors/**/*.md",
+				typeName: "Authors",
+				remark: {
+					plugins: [
+						// ...local plugins
+					]
+				}
+			}
+		},
+		{
+			use: "@gridsome/source-filesystem",
+			options: {
+				path: "contents/posts/**/*.md",
+				typeName: "Posts",
+				refs: {
+					author: "Authors"
+				},
+				remark: {
+					plugins: [
+						// ...local plugins
+					]
+				}
+			}
+		}
+	]
+};
