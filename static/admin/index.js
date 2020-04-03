8f5f400c46675ac1598b7c9bfd2824897ddd5877
Marasi <marasi.mbp@gmail.com>
2020-04-03 12:58:59 +0700

    Add id

--- static/admin/index.js
+++ static/admin/index.js
@@ -1,6 +1,36 @@
-console.log("LOADED");
+import CMS from "netlify-cms";
 import uuid from "uuid/v4";
-import React from "react";
 
-console.log(uuid);
-console.log(React);
+const IdControl = window.createClass({
+	getInitialState: function() {
+		return {};
+	},
+	componentDidMount: function() {
+		// If this widget doesn't have an ID yet we create one using the UUID package
+		if (!this.props.value) {
+			this.props.onChange(uuid());
+		}
+	},
+	handleChange() {
+		this.props.onChange(uuid());
+	},
+	render: function() {
+		return window.h("p", null, `${this.props.value}`);
+	}
+});
+
+/**
+ * Create the preview widget, this will display the widgets value in the NetlifyCMS preview pane
+ */
+const IdPreview = window.createClass({
+	getInitialState: function() {
+		console.log(this.props);
+		return {};
+	},
+	render: function() {
+		return window.h("p", null, `ID: ${this.props.value}`);
+	}
+});
+
+// Register the widget. This lets NetlifyCMS know about our custom widget
+CMS.registerWidget("id", IdControl, IdPreview);
